1bb31b0f4a5e20c53ad9bf121cf0260445f3884a
KrulesExGamer <57680132+PedroVNasc@users.noreply.github.com>
2024-06-15 01:24:01 -0300

    MPV pronto

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -1,14 +1,14 @@
-uniform sampler2D samplerTexture;
+uniform sampler2D sampler_texture;
 
-// uniform vec3 lightPos; // define coordenadas de posicao da luz
+// uniform vec3 light_pos; // define coordenadas de posicao da luz
 uniform float ka; // coeficiente de reflexao ambiente
 uniform float kd; // coeficiente de reflexao difusa
 uniform float ks; // coeficiente de reflexao difusa
 uniform float ns; // coeficiente de reflexao difusa
-uniform vec3 viewPos;
 
-vec3 baseLight = vec3(1.0, 1.0, 1.0);
-// vec3 lightColor = vec3(1.0, 1.0, 1.0);
+uniform vec3 view_pos;
+
+vec3 base_light = vec3(1.0, 1.0, 1.0);
 
 #define MAX_LIGHTS 10
 uniform vec3 lights_position[MAX_LIGHTS];
@@ -17,41 +17,56 @@ uniform float lights_intensity[MAX_LIGHTS];
 uniform float lights_reach[MAX_LIGHTS];
 uniform int index_max;
 
-varying vec3 out_fragPos; // recebido do vertex shader
+// Luz que acompanha o lobo
+uniform vec3 wolf_position;
+uniform vec3 wolf_color;
+uniform float wolf_intensity;
+uniform float wolf_reach;
+
+varying vec3 out_position; // recebido do vertex shader
 varying vec2 out_texture;
 varying vec3 out_normal; // recebido do vertex shader
 
+
+vec3 phong(vec3 position, vec3 color, float reach, float intensity);
+
 void main(){
-    vec3 ambient = ka * baseLight;             
+    vec3 ambient = ka * base_light;             
 
     vec3 sum = ambient; // aplica iluminacao
 
     for (int i = 0; i < index_max; i++) {
-        vec3 lightPos = lights_position[i];
-        vec3 lightColor = lights_color[i];
-        float reach = lights_reach[i];
-
-        vec3 norm = normalize(out_normal); // normaliza vetores perpendiculares
-        vec3 lightDir = normalize(lightPos - out_fragPos); // direcao da luz
-        float diff = max(dot(norm, lightDir), 0.0); // verifica limite angular (entre 0 e 90)
-
-        float dist = distance(lightPos, out_fragPos);
-        float fading = max((reach - dist), 0.0);
-
-        vec3 diffuse = kd * diff * fading * lightColor; // iluminacao difusa
-    
-        vec3 viewDir = normalize(viewPos - out_fragPos); // direcao do observador/camera
-        vec3 reflectDir = reflect(-lightDir, norm); // direcao da reflexao
-
-        float spec = pow(max(dot(viewDir, reflectDir), 0.0), ns);
-        
-        vec3 specular = ks * spec* fading * lightColor;
-
-        sum += diffuse + specular;
+        sum += phong(
+            lights_position[i], 
+            lights_color[i], 
+            lights_reach[i],
+            lights_intensity[i]
+        );
     }
 
+    sum += phong(wolf_position, wolf_color, wolf_reach, wolf_intensity);
     
-    vec4 texture = texture2D(samplerTexture, out_texture);
+    vec4 texture = texture2D(sampler_texture, out_texture);
     vec4 result = vec4(sum, 1.0) * texture; // aplica iluminacao
     gl_FragColor = result;
+}
+
+vec3 phong(vec3 position, vec3 color, float reach, float intensity) {
+    vec3 N = normalize(out_normal); 
+    vec3 L = normalize(position - out_position); // direcao da luz
+    float diff = max(dot(N, L), 0.0); // verifica limite angular (entre 0 e 90)
+
+    float dist = distance(position, out_position);
+    float fading = max((reach - dist), 0.0);
+
+    vec3 diffuse = kd * diff * fading * color; // iluminacao difusa
+
+    vec3 V = normalize(view_pos - out_position); // direcao do observador/camera
+    vec3 R = reflect(-L, N); // direcao da reflexao
+
+    float spec = pow(max(dot(V, R), 0.0), ns);
+    
+    vec3 specular = ks * spec * fading * color;
+
+    return diffuse + specular;
 }
--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -3,7 +3,7 @@ attribute vec2 texture_coord;
 attribute vec3 normals;
 
 varying vec2 out_texture;
-varying vec3 out_fragPos;
+varying vec3 out_position;
 varying vec3 out_normal;
         
 uniform mat4 model;
@@ -13,6 +13,6 @@ uniform mat4 projection;
 void main(){
     gl_Position = projection * view * model * vec4(position,1.0);
     out_texture = vec2(texture_coord);
-    out_fragPos = vec3(model * vec4(position, 1.0));
+    out_position = vec3(model * vec4(position, 1.0));
     out_normal = normals;            
 }
